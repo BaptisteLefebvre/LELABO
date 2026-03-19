9ee2d086fe2977792073e626b0f22ff04082fc03
Baptiste_Lefebvre <Baptiste.lefebvre2002@gmail.com>
2026-03-19 16:18:15 +0100

    Finalisation du menu de navigation et tri des fichiers

--- services/script-services.js
+++ services/script-services.js
@@ -19,6 +19,38 @@ let services = document.querySelectorAll(".sections2")
 
 gsap.registerPlugin(ScrollTrigger)
 
+const links = document.querySelectorAll(".link");
+const arrow = document.querySelector(".arrow");
+
+// Hover → déplacer la flèche
+links.forEach(link => {
+  link.addEventListener("mouseenter", () => {
+    link.prepend(arrow);
+    link.classList.add("active");
+
+    if (link.textContent.trim() === "NOTRE MÉTHODE") {
+        link.classList.add("small");
+    }
+  });
+});
+
+// Revenir sur le lien actif
+const activeLink = document.querySelector(".link-active");
+
+document.querySelectorAll(".link").forEach(link => {
+  link.addEventListener("mouseleave", () => {
+    if (activeLink) {
+      activeLink.prepend(arrow);
+    }
+    
+    link.classList.remove("active");
+    
+    if (link.textContent.trim() === "NOTRE MÉTHODE") {
+        link.classList.remove("small");
+    }
+  });
+});
+
 btn_nav_hide.addEventListener("click", () => {
     navbar.classList.remove("open")
     setTimeout(() => {
